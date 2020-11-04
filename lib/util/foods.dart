1bdd1eb42ddfd1794b1d8f037baf39df37a501ad
Abhinavnarula <abhinavnarula3@gmail.com>
2020-11-04 19:25:39 +0530

    initial commit

---
+++ lib/util/foods.dart
@@ -0,0 +1,50 @@
+List<Map> foods = [
+  {
+    "img": "assets/food1.jpeg",
+    "name": "Apple",
+  },
+  {
+    "img": "assets/food2.jpg",
+    "name": "Banana",
+  },
+  {
+    "img": "assets/food3.jpg",
+    "name": "Cauliflower",
+  },
+  {
+    "img": "assets/food4.jpg",
+    "name": "Papaya",
+  },
+  {
+    "img": "assets/food5.jpg",
+    "name": "Cauliflower",
+  },
+  {
+    "img": "assets/food6.jpg",
+    "name": "Cabbage",
+  },
+  {
+    "img": "assets/food7.jpg",
+    "name": "Grapes",
+  },
+  {
+    "img": "assets/food8.jpg",
+    "name": "Potato",
+  },
+  {
+    "img": "assets/food9.jpg",
+    "name": "Okra",
+  },
+  {
+    "img": "assets/food10.jpg",
+    "name": "Grapes",
+  },
+  {
+    "img": "assets/food11.jpg",
+    "name": "Grapes",
+  },
+  {
+    "img": "assets/food12.jpg",
+    "name": "Okra",
+  },
+];
